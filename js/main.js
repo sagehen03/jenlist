e8330fbe14f930facdf9c35e0cde068cc8e5f358
Jonathan Kim <me@jonathan-kim.com>
2015-08-30 13:00:35 -0400

    Basic styling for adding an item.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ var Backbone = require('backbone');
 Backbone.$ = $;
 var MasterList = require('./views/master-list');
 var NewMasterListItem = require('./views/new-master-list-item');
-var ml = new MasterList();
-new NewMasterListItem({masterCollection: ml.collection});
 
+
+var masterListView = new MasterList();
+var addItemView = new NewMasterListItem({masterCollection: masterListView.collection});
+
+masterListView.render();
+addItemView.render();
 
--- js/views/new-master-list-item.js
+++ js/views/new-master-list-item.js
@@ -1,20 +1,17 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
 var $ = require('jquery');
-var newItemTemplate = require('../views/templates/item.jade');
+var newItemTemplate = require('../views/templates/add-item.jade');
 
 var NewMasterListItemView = Backbone.View.extend({
-    el:  '#newItemArea',
+    el:  '#add-item',
 
     events: {
-        'keypress #newItem': 'createOnEnter'
+        'keypress input': 'createOnEnter'
     },
 
     initialize: function(options){
         this.masterCollection = options.masterCollection;
-        this.render();
-        this.$input = $('#newItem');
-        console.log(this.$input);
     },
 
     render: function () {
@@ -23,7 +20,7 @@ var NewMasterListItemView = Backbone.View.extend({
     },
 
     createOnEnter: function (e) {
-        if (e.which === 13 && this.$input.val().trim()) {
+        if (e.which === 13 && this.$('input').val().trim()) {
             console.log("Hello!");
             this.masterCollection.create({name: this.$input.val().trim()});
             this.$input.val('');
