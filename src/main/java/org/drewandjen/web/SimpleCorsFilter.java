a9ad29204df9f0c907231e4db67cb9108f2014ce
dhite <dhite@smartertravelmedia.com>
2015-08-30 15:29:59 -0400

    access control allow headers

--- src/main/java/org/drewandjen/web/SimpleCorsFilter.java
+++ src/main/java/org/drewandjen/web/SimpleCorsFilter.java
@@ -15,6 +15,7 @@ public class SimpleCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         chain.doFilter(req, res);
     }
 
